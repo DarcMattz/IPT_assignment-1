62b10cc7a65943f33445020da3f8b2245789c738
DarcMattz <jcarvajal_21UR0148@psu.edu.ph>
2024-02-12 10:21:14 +0800

    first commit

--- Item2/Program.cs
+++ Item2/Program.cs
@@ -5,10 +5,23 @@
         static void Main(string[] args)
         {
             double deposite, withrawal;
-            Console.Write("Deposite (1-2000): ");
+
+            do
+            {
+                Console.Write("Deposite (1-2000): ");
                 deposite = double.Parse(Console.ReadLine());
-            Console.Write("Withdrawal (1-2000): ");
+                Console.Write("Withdrawal (1-2000): ");
                 withrawal = double.Parse(Console.ReadLine());
+                if (deposite >= 1 && deposite <= 2000)
+                {
+                    if(withrawal >= 1 && withrawal <= 2000)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("Input should be 1-2000 only!");
+            } while (true);
+
             compute(deposite, withrawal);
         }
 
